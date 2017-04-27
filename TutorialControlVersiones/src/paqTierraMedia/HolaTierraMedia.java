8c73058321fde353439fa9a8eb3e435913c515ce
Cristina2 <Cristina2@CristinaPC>
2017-04-27 23:19:41 +0200

    Añado mas nombres y cambio el syso

--- TutorialControlVersiones/src/paqTierraMedia/HolaTierraMedia.java
+++ TutorialControlVersiones/src/paqTierraMedia/HolaTierraMedia.java
@@ -4,13 +4,13 @@ public class HolaTierraMedia {
 		
 		public static void main(String[] args){
 			
-			String[] nombres={"Bilbo","Frodo","Aragorn","Wilson"};
+			String[] nombres={"Bilbo","Frodo","Aragorn","Wilson","Legolas","Faramir","Gandalf"};
 
 		//saludos 
 			
 		for (int i=0;i<nombres.length;i++){
 			System.out.println("Hola, "+nombres[i]);
-			System.out.println("¿Como estás hoy?");
+			System.out.println("¿Qúe tal lo estás pasando hoy?");
 		}
 		}	
 	}
