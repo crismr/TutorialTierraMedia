9a284865ff96b529fafc5e64e3c17fb0f3fa073d
Cristina <Cristina@CristinaPC>
2017-04-27 23:09:23 +0200

    Inicial, creando y sincronizando repositorio

---
+++ TutorialControlVersiones/src/paqTierraMedia/HolaTierraMedia.java
@@ -0,0 +1,17 @@
+package paqTierraMedia;
+
+public class HolaTierraMedia {
+		
+		public static void main(String[] args){
+			
+			String[] nombres={"Bilbo","Frodo","Aragorn"};
+
+		//saludos 
+			
+		for (int i=0;i<nombres.length;i++){
+			System.out.println("Hola, "+nombres[i]);
+			System.out.println("¿Como estás hoy?");
+		}
+		}	
+	}
+
